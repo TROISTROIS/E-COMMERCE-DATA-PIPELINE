943485d1b7264e080f529c0f5dec80c8eb4a7d80
TROIS <franciscangeno1@gmail.com>
2024-05-23 17:59:09 +0300

    Fixed date error

--- upload_to_s3.py
+++ upload_to_s3.py
@@ -3,12 +3,12 @@ import boto3
 s3 = boto3.client("s3")
 bucket_name = "e-commerce-bucket-fn"
 
-def upload_to_s3(filename, date_str):
+def upload_to_s3(filename, current_date):
     """Uploads a CSV file to S3 with Hive-style partitioning"""
     # Extract year, month, day from current_date
-    year = date_str.year
-    month = date_str.month
-    day = date_str.day
+    year = current_date.year
+    month = current_date.month
+    day = current_date.day
     file_path = f"transactions/year={year}/month={month}/day={day}/{filename}"  # Hive-style partitioning
     s3.upload_file(f'/tmp/{filename}', bucket_name, file_path)
     print(f"File uploaded to S3 Bucket {bucket_name}/{file_path}")
