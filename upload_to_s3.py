f783af15321be4ed9b2af4a08425ad93fda42294
TROIS <franciscangeno1@gmail.com>
2024-05-24 17:48:44 +0300

    Changed filepath

--- upload_to_s3.py
+++ upload_to_s3.py
@@ -15,11 +15,11 @@ def upload_to_s3(filename, current_date):
     file_path2 = f"raw_data/"
     for file in files[:-1]:
         file_path1 = f"transactions/year={year}/month={month}/day={day}/{filename}"  # Hive-style partitioning
-        upload_to_s3(f"/tmp/{filename}", bucket_name, file_path1)
+        s3.upload_file(f'/tmp/{filename}', bucket_name, file_path1)
     # Upload the last file to a different path
     filename = files[-1]
-    file_path2 = f"raw_data/{filename}"
-    upload_to_s3(f"/tmp/{filename}", bucket_name, file_path2)
+    file_path2 = f'raw_data/{filename}'
+    s3.upload_file(f'/tmp/{filename}', bucket_name, file_path2)
 
     print(f"File uploaded to S3 Bucket {bucket_name}/{file_path2}")
     return
